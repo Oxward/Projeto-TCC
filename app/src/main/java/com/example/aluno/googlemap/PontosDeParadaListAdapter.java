8785d486897b01c47319a59d43ea056b89fc6b92
Oxward <andersonfreiretads@gmail.com>
2018-08-24 14:58:19 -0300

    ListView

--- app/src/main/java/com/example/aluno/googlemap/PontosDeParadaListAdapter.java
+++ app/src/main/java/com/example/aluno/googlemap/PontosDeParadaListAdapter.java
@@ -2,7 +2,16 @@ package com.example.aluno.googlemap;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -11,11 +20,71 @@ import java.util.List;
 class PontosDeParadaListAdapter extends ArrayAdapter<PontosDeParada>
 {
     private static final String TAG = "PontosDeParadaListAdapt";
-    private Context context;
+    private Context mContext;
+    private int mResource;
+    private int lastPosition = -1;
 
+    static class ViewHolder
+    {
+        TextView hora;
+        TextView parada;
+    }
 
     public PontosDeParadaListAdapter(@NonNull Context context, int resource, @NonNull ArrayList<PontosDeParada> objects)
     {
         super(context, resource, objects);
+        mContext = context;
+        mResource = resource;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        //Informações dos pontos
+        String hora = getItem(position).getHora();
+        String parada = getItem(position).getParada();
+
+        //Cria um objeto ponto de parada
+        PontosDeParada pontosDeParada = new PontosDeParada(hora, parada);
+
+        //Cria uma view para "mostrar" a animação
+        final View result;
+        //Objeto ViewHolder
+        ViewHolder holder;
+
+
+        if( convertView == null)
+        {
+
+            LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(mResource, parent, false);
+
+            //Código android developers
+            holder = new ViewHolder();
+            holder.hora = (TextView) convertView.findViewById(R.id.txtViewHora);
+            holder.parada = (TextView) convertView.findViewById(R.id.txtViewPontoPar);
+            result = convertView;
+            convertView.setTag(holder);
+        }
+        else
+        {
+            holder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+        Animation animation = AnimationUtils.loadAnimation(mContext,
+                (position > lastPosition) ? R.anim.load_down_anim : R.anim.load_up_anim );
+        result.startAnimation(animation);
+        lastPosition = position;
+
+        holder.hora.setText(pontosDeParada.getHora());
+        holder.parada.setText(pontosDeParada.getParada());
+
+//        TextView txHora = convertView.findViewById(R.id.txtViewHora);
+//        TextView txPontos = convertView.findViewById(R.id.txtViewHora);
+//        txHora.setText(hora);
+//        txPontos.setText(parada);
+
+        return convertView;
     }
 }
