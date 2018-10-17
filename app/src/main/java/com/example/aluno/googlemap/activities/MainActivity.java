efd11c83dd512aafb326cd4251fdb2cf597e3b35
Oxward <andersonfreiretads@gmail.com>
2018-10-17 17:11:17 -0300

    Changes

--- app/src/main/java/com/example/aluno/googlemap/activities/MainActivity.java
+++ app/src/main/java/com/example/aluno/googlemap/activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aluno.googlemap.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -9,10 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.aluno.googlemap.R;
+import com.example.aluno.googlemap.lists.Lists;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -72,22 +76,37 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (id) {
+            case R.id.nav_routes:
+                try {
+                    Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Exceção", "" + e.getMessage());
+                }
+                break;
 
-        } else if (id == R.id.nav_slideshow) {
+            case R.id.nav_schedule:
+                try {
+                    Intent intent = new Intent(MainActivity.this, Lists.class);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("Exceção", "" + e.getMessage());
+                }
+                break;
 
-        } else if (id == R.id.nav_manage) {
+            case R.id.nav_manage:
+                break;
 
-        } else if (id == R.id.nav_share) {
+            case R.id.nav_share:
+                break;
 
-        } else if (id == R.id.nav_send) {
-
+            case R.id.nav_send:
+                break;
         }
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
