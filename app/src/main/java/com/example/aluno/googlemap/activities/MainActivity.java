969bf103f08b710ef284923e4fe4b5e7846d4ef6
Oxward <andersonfreiretads@gmail.com>
2018-11-09 12:52:00 -0200

    as

--- app/src/main/java/com/example/aluno/googlemap/activities/MainActivity.java
+++ app/src/main/java/com/example/aluno/googlemap/activities/MainActivity.java
@@ -102,7 +102,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme_NoActionBar);
+        //setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
